dc498235ce2720e73acb0df96abc9aaf6461c039
advanced-garbage <94859510+advanced-garbage@users.noreply.github.com>
2022-02-24 13:06:01 +0200

    Add files via upload

---
+++ FishingNet/Fish.cs
@@ -0,0 +1,19 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {
+        public string FishType { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+        public Fish(string fishtype, double length, double weight)
+        {
+            this.FishType = fishtype;
+            this.Length = length;
+            this.Weight = weight;
+        }
+        public override string ToString()
+        {
+            return $"There is a {this.FishType}, {this.Length} cm. long, and {this.Weight} gr. in weight.";
+        }
+    }
+}
---
+++ FishingNet/Net.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        private List<Fish> fish { get; set; }
+        public IReadOnlyCollection<Fish> Fish { get { return this.fish; } }
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return fish.Count; } }
+        public Net(string material, int cap)
+        {
+            this.Material = material;
+            this.Capacity = cap;
+            this.fish = new List<Fish>();
+        }
+
+        public string AddFish(Fish fish)
+        {
+            bool meetsCriteria = (!string.IsNullOrWhiteSpace(fish.FishType) &&
+                                 (fish.Length > 0 && fish.Weight > 0));
+            if (!meetsCriteria)
+                return "Invalid fish.";
+
+            bool isThereRoom = this.fish.Count < this.Capacity;
+            if (!isThereRoom)
+                return "Fishing net is full.";
+
+            this.fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+            bool doesExist = this.fish.Any(x => x.Weight.Equals(weight));
+            if (doesExist)
+            {
+                this.fish.Remove(this.fish.Find(x => x.Weight.Equals(weight)));
+                return true;
+            }
+            else return false;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            bool doesExist = this.fish.Any(x => x.FishType.Equals(fishType));
+            if (doesExist)
+                return this.fish.Find(x => x.FishType.Equals(fishType));
+            else
+                return null;
+        }
+
+        public Fish GetBiggestFish()
+        {
+            if (this.fish.Count > 0)
+                return this.fish.OrderBy(x => x.Length).Last();
+            else
+                return null;
+        }
+
+        public string Report()
+        {
+            var report = new StringBuilder();
+            report.AppendLine($"Into the {this.Material}:");
+            foreach (var fi in fish.OrderByDescending(x => x.Length))
+                report.AppendLine($"{fi}");
+            return report.ToString().TrimEnd();
+        }
+    }
+}
